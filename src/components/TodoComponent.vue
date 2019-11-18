9bba202d66d5a6376dc03e40b52b1fee25b253a7
Bachir Ndiaye <ndiayeb44@gmail.com>
2019-11-18 10:52:04 +0000

    Vuex Store implemented

--- src/components/TodoComponent.vue
+++ src/components/TodoComponent.vue
@@ -3,7 +3,8 @@
 		<input type="text" v-model="todoToAdd"><button @click="PUSH_AddTodo">Add</button>
 
 		<div>
-			<div @click="toggleCompleted({ id: todo.id })" v-for="todo in todos" :key="todo.id" :class="{ done : todo.done }">{{ todo.id }} | {{ todo.todo }} | {{ todo.done ? 'Done' : 'Not completed' }} |
+			<div @click="UPDATE_toggleCompleted({ id: todo.id })" v-for="todo in todos" :key="todo.id" :class="{ done : todo.done }">
+				{{ todo.id }} | {{ todo.todo }} | {{ todo.done ? 'Done' : 'Not completed' }} |
 				<span @click="deleteTodo({ id: todo.id })" > X </span>
 			</div>
 		</div>
@@ -17,7 +18,7 @@
 </template>
 
 <script>
-import { mapGetters, mapMutations } from 'vuex'
+import { mapGetters, mapActions/*, mapMutations*/ } from 'vuex'
 
 export default {
 	name: 'TodoComponent',
@@ -32,7 +33,7 @@ export default {
 			if (toDisplay == 'done') this.todos = this.getDoneTodos
 			else if (toDisplay == 'uncompleted') this.todos = this.getUncompletedTodos
 			else if (toDisplay == 'all') this.todos = this.getTodos
-		},
+		},/*
 		//Pour utiliser les mutations on peut soit faire ça
 		PUSH_AddTodo() {
 			//this.$store.commit('addTodo', { todo: this.todoToAdd });
@@ -42,8 +43,35 @@ export default {
 				todo: this.todoToAdd
 			});
 		},
-		//Oubien utiliser mapMutations
+		//Oubien utiliser mapMutations. Dans ce cas le commit est fait automatiquement
 		...mapMutations(['toggleCompleted', 'deleteTodo'])
+		*/
+		/**
+		 * Mais la manière adéquate est d'utiliser les actions pour propager les actions dont on a besoin
+		 * les actions se chargenront de faire le commit des mutations
+		 * Cela nous permettra d'utiliser des operations asynchrones au niveau des actions
+		 * ensuite faire un commit de la mutation vu qu'il ne faut pas faire d'operations
+		 * asynchrones au niveau des mutations
+		 */
+		PUSH_AddTodo() {
+			//this.$store.dispatch('addTodo', { todo: this.todoToAdd });
+			//or
+			this.$store.dispatch({
+				type: 'addTodo', //L'action à dispatcher
+				todo: this.todoToAdd
+			});
+		},
+		UPDATE_toggleCompleted(payload) {
+			this.$store.dispatch('toggleCompleted', payload)
+				.then(response => {
+					window.console.log(response);
+				})
+				.catch(err => {
+					window.console.error(err);
+				})
+		},
+		//On peut toujours utiliser mapActions pour aller vite
+		...mapActions(['deleteTodo']),
 	},
 	computed: {
 		doSomething() { //juste pour montrer qu'on peut mettre d'autres valeurs computed et mettre les getters avec ...mapGetters. Au lieu de mettre directement computed: mapGetters([...], [...]). Cette derniere peut etre utilisée si les donnes du getters sont les seules donnees computed pour ce composant
--- src/store/index.js
+++ src/store/index.js
@@ -7,7 +7,7 @@ export default new Vuex.Store({
 	state: {
 		todos: [],
 	},
-	mutations: {
+	mutations: { /** IMPORTANT: mutations must not have ascynchronous operation. If we want to call an API for example, we have to do it using actions then we commit a mutation */
 		addTodo: (state, payload) => {
 			if (state.todos[0]) {
 				let newTodoId = state.todos.reduce((maxId, todo) => todo.id > maxId ? todo.id : maxId, state.todos[0].id) + 1;
@@ -20,6 +20,40 @@ export default new Vuex.Store({
 			state.todos.splice(state.todos.findIndex(todo => todo.id == payload.id), 1);
 		},
 		toggleCompleted: (state, payload) => state.todos.filter(todo => todo.id == payload.id)[0].done = !state.todos.filter(todo => todo.id == payload.id)[0].done
+	},
+	actions: {
+		/**
+		 * - Context is passed to actions and it is an object that contains
+		 * all the properties of state. We dispatch an action when needed
+		 * and it commit a mutations
+		 * - It is not possible to do an asynchronous operation inside mutations
+		 * but we can handle asynchronous here inside the actions and commit mutation
+		 * when our asynchrounous operation is resolved. It's more powerful when we
+		 * use 'async' and 'await'
+		 */
+		addTodo(context, payload) {
+			//We simulate asynchronous operation
+			setTimeout(() => {
+				context.commit('addTodo', payload);
+			}, 1000);
+		},
+		deleteTodo({ commit }, payload) { //Ici on utilise la destructuration d'argument afin de pouvoir utiliser directement commit sans faire contexr.commit
+			setTimeout(() => {
+				commit('deleteTodo', payload);
+			}, 1000);
+		},
+		toggleCompleted({ commit }, payload) {
+			return new Promise((resolve, reject) => {
+				setTimeout(() => {
+					if (payload) {
+						commit('toggleCompleted', payload);
+						resolve(`Todo ${payload.id} state changed`);
+					} else {
+						reject(`Error while updated todo ${payload.id} state: NO payload`);
+					}
+				}, 1000);
+			});
+		}
 	},
 	getters: {
 		getTodos: state => state.todos,
